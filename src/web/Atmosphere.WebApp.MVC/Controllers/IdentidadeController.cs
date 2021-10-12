ab8277b589ea73639deec09b810072af97d71591
Jeff-vinicius <jeffersonshady10@gmail.com>
2021-10-12 11:24:22 -0300

    Controller, Views, Models de login

--- src/web/Atmosphere.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/Atmosphere.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Atmosphere.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,56 @@ namespace Atmosphere.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            // API - Registro
+
+            if (false) return View(usuarioRegistro);
+
+            // Realizar login 
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            // API - Login
+
+            if (false) return View(usuarioLogin);
+
+            // Realizar login 
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
