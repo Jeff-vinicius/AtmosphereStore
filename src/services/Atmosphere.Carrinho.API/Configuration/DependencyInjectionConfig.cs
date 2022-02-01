cb70b80c907bc73a5663682fc1a4019caf3cedbd
Jeff-vinicius <jeffersonshady10@gmail.com>
2022-02-01 15:28:13 -0300

    API de Carrinho - Base inicial e Estrutura de Dados

---
+++ src/services/Atmosphere.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using Atmosphere.WebApp.MVC.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Atmosphere.Carrinho.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //services.AddScoped<AspNetUser, AspNetUser>();
+            //services.AddScoped<CarrinhoContext>();
+        }
+    }
+}
---
+++ src/services/Atmosphere.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,10 @@
+﻿using Atmosphere.WebAPI.Core.Controllers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Atmosphere.Carrinho.API.Controllers
+{
+    [Authorize]
+    public class CarrinhoController : MainController
+    {
+    }
+}
